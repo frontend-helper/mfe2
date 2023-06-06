39dcef0e0d7ab743cb2122bfb0cda4b90bca22b7
frontend-helper <108347339+frontend-helper@users.noreply.github.com>
2023-06-06 11:04:31 +0530

    MFE two code changes.

---
+++ concat.js
@@ -0,0 +1,10 @@
+const concat = require("concat");
+(async function build() {
+  const files = [
+    "dist/mfe2/main.js",
+    "dist/mfe2/polyfills.js",
+    "dist/mfe2/runtime.js",
+  ];
+
+  await concat(files, "dist/mfe2/mfe-two.js");
+})();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { createCustomElement } from '@angular/elements';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  //bootstrap: [AppComponent],
+  entryComponents: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const el = createCustomElement(AppComponent, {
+      injector: this.injector,
+    });
+    customElements.define('mfe-two', el);
+  }
+}
